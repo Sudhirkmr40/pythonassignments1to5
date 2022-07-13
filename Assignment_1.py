1583a83e184711b2e44d041f4a38abe944dc5ffe
Kumar <sudhir.6.kumar.ext@nokia.com>
2022-07-13 22:54:16 +0530

    Initial commit

---
+++ Assignment_1.py
@@ -0,0 +1,27 @@
+1. In the below elements which of them are values or an expression? eg:- values can be integer or string and expressions will be mathematical operators.
+* - expression
+'hello' - string
+-87.8 - float
+-  mathmatical operator
+/ mathmatical operator
++ mathmatical operator
+6 int
+
+#
+2. What is the difference between string and variable?
+# a variable can store string ,it is entity which can store data , while string is collection of alphabetical characters
+# enclosed by single & double invertrd commas.
+
+
+#3. Describe three different data types.
+# 1.string - tring is collection of alphabetical characters, enclosed by single & double invertrd commas.
+# 2. Dictionary - it is collection of elements with key value pairs , enlosed by curly brackets.
+# 3, tuple - tuple is ordered collection of elements of different datatypes, and these are immutable.
+
+
+#4. What is an expression made up of? What do all expressions do?
+# expressions are made up of values & mathematical operators , constants, and variables. expression combines to produce results on screen
+
+
+#5. This assignment statements, like spam = 10. What is the difference between an expression and a statement?
+#
