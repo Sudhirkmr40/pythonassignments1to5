6e46fdeb89598b0c24dee3be806456d8af25748f
Kumar <sudhir.6.kumar.ext@nokia.com>
2022-07-14 10:32:31 +0530

    Initial commit

--- Assignment_1.py
+++ Assignment_1.py
@@ -1,12 +1,11 @@
-1. In the below elements which of them are values or an expression? eg:- values can be integer or string and expressions will be mathematical operators.
-* - expression
+#1. In the below elements which of them are values or an expression? eg:- values can be integer or string and expressions will be mathematical operators.
+#(* - expression
 'hello' - string
 -87.8 - float
 -  mathmatical operator
 / mathmatical operator
 + mathmatical operator
-6 int
-
+6  int))
 #
 2. What is the difference between string and variable?
 # a variable can store string ,it is entity which can store data , while string is collection of alphabetical characters
@@ -24,4 +23,27 @@
 
 
 #5. This assignment statements, like spam = 10. What is the difference between an expression and a statement?
-#
+# expressions are made up of values & mathematical operators , constants, and variables. expression combines to produce results on screen
+# statement is command that a python interpreter executes like print
+
+#6. After running the following code, what does the variable bacon contain?
+bacon = 22
+bacon + 1
+print(bacon+1) = 23
+
+# 7. What should the values of the following two terms be?
+# 'spam' + 'spamspam'
+# 'spam' * 3
+#ANS - spamspamspam  & spamspamspam
+
+8. Why is eggs a valid variable name while 100 is invalid?
+# A varialble name cannt start with a number or integer
+
+# 9. What three functions can be used to get the integer, floating-point number, or string version of a value?
+ans: str() , int(),  float()
+
+10. Why does this expression cause an error? How can you fix it?
+'I have eaten ' + 99 + ' burritos.'
+Ans - print( 'I have eaten ' + 99 + ' burritos.')
+TypeError: can only concatenate str (not "int") to str
+
